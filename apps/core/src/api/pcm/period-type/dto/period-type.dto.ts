e8b1f82cf34c644656cb9b858664facf519122f5
bahram <bahram.rajabiws@gmail.com>
2023-11-07 16:30:19 +0330

    add ignore paging query and refactor

--- apps/core/src/api/pcm/period-type/dto/period-type.dto.ts
+++ apps/core/src/api/pcm/period-type/dto/period-type.dto.ts
@@ -1,8 +1,7 @@
 import { IntersectionType } from '@nestjs/swagger';
-import { PeriodTypeFilter } from '../filter';
-import { ListFilter } from 'apps/core/src/util/core/query';
+import { IgnorePagingFilter, ListFilter } from 'apps/core/src/util/core/query';
 
 export class PeriodTypeGetDto extends IntersectionType(
-  PeriodTypeFilter,
+  IgnorePagingFilter,
   ListFilter,
 ) {}
--- apps/core/src/util/core/query/index.ts
+++ apps/core/src/util/core/query/index.ts
@@ -1,1 +1,2 @@
 export * from './list-filter.query';
+export * from './ignore-paging.query';
