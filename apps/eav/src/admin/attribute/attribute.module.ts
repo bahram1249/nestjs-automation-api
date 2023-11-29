0d47d30e7e31a45ab1eb1827fbc55e4c52ce0530
bahram <bahram.rajabiws@gmail.com>
2023-11-29 14:10:05 +0330

    fix module name

--- apps/eav/src/admin/attribute/attribute.module.ts
+++ apps/eav/src/admin/attribute/attribute.module.ts
@@ -6,7 +6,7 @@ import { User } from '@rahino/database/models/core/user.entity';
 import { Permission } from '@rahino/database/models/core/permission.entity';
 import { EAVAttribute } from '@rahino/database/models/eav/eav-attribute.entity';
 import { EAVEntityAttribute } from '@rahino/database/models/eav/eav-entity-attribute.entity';
-import { AttributeProfile } from './mapper/attribute.mapper';
+import { AttributeProfile } from './mapper';
 import { EAVAttributeType } from '@rahino/database/models/eav/eav-attribute-type.entity';
 import { EAVEntityType } from '@rahino/database/models/eav/eav-entity-type.entity';
 
