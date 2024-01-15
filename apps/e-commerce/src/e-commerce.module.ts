c3c0bab99897bf55c15effc3da756bba34341c9d
bahram <bahram.rajabiws@gmail.com>
2024-01-15 16:25:04 +0330

    inital swagger api for ecommerce module

--- apps/e-commerce/src/e-commerce.module.ts
+++ apps/e-commerce/src/e-commerce.module.ts
@@ -1,8 +1,32 @@
-import { Module } from '@nestjs/common';
+import {
+  INestApplication,
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+} from '@nestjs/common';
 import { ProductModule } from './admin/product/product.module';
 import { LoginModule } from './user/login/login.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 @Module({
   imports: [LoginModule, ProductModule],
 })
-export class ECommerceModule {}
+export class ECommerceModule implements NestModule {
+  constructor() {}
+  private app: INestApplication;
+  configure(consumer: MiddlewareConsumer) {}
+  setApp(app: INestApplication<any>) {
+    this.app = app;
+    const coreConfig = new DocumentBuilder()
+      .setTitle('ECommerce Api')
+      .setDescription('The ECommerce API description')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+    const coreDocument = SwaggerModule.createDocument(this.app, coreConfig, {
+      include: [ECommerceModule],
+      deepScanRoutes: true,
+    });
+    SwaggerModule.setup('api/ecommerce', this.app, coreDocument);
+  }
+}
