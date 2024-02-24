1637dacb399220ab7da84c72d66008ea7ebb025c
bahram rajabi <bahram.rajabiws@gmail.com>
2024-02-24 22:09:07 +0330

    initial session guard

--- apps/e-commerce/src/user/session/session.module.ts
+++ apps/e-commerce/src/user/session/session.module.ts
@@ -3,10 +3,13 @@ import { SessionService } from './session.service';
 import { SessionController } from './session.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { ECUserSession } from '@rahino/database/models/ecommerce-eav/ec-user-session.entity';
+import { SessionGuard } from './guard/session.guard';
+import { RedisClientModule } from '@rahino/redis-client';
 
 @Module({
-  imports: [SequelizeModule.forFeature([ECUserSession])],
+  imports: [SequelizeModule.forFeature([ECUserSession]), RedisClientModule],
   controllers: [SessionController],
-  providers: [SessionService],
+  providers: [SessionService, SessionGuard],
+  exports: [SessionGuard],
 })
 export class SessionModule {}
