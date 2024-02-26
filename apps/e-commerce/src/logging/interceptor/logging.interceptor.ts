f1ebd99c906fdf60d2d612f8b96fef7153af0bfc
bahram rajabi <bahram.rajabiws@gmail.com>
2024-02-26 13:55:52 +0330

    add auto remove of job

--- apps/e-commerce/src/logging/interceptor/logging.interceptor.ts
+++ apps/e-commerce/src/logging/interceptor/logging.interceptor.ts
@@ -35,7 +35,15 @@ export class LoggingInterceptor implements NestInterceptor {
             },
             session: request.ecsession,
           },
-          { keepLogs: 5000 },
+          {
+            removeOnComplete: {
+              age: 3600,
+              count: 100,
+            },
+            removeOnFail: {
+              age: 24 * 3600,
+            },
+          },
         );
       }),
     );
