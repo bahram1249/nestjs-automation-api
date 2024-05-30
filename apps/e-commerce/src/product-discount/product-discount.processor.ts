c0b89b73995d1a98ecd2141860735fa1bb19b607
bahram rajabi <bahram.rajabiws@gmail.com>
2024-05-30 15:26:55 +0330

    disable job in development

--- apps/e-commerce/src/product-discount/product-discount.processor.ts
+++ apps/e-commerce/src/product-discount/product-discount.processor.ts
@@ -4,6 +4,7 @@ import { DBLogger } from '@rahino/logger';
 import { PRODUCT_DISCOUNT_QUEUE } from './constansts';
 import { ProductDiscountService } from './product-discount.service';
 import { ListFilterV2Factory } from '@rahino/query-filter/provider/list-filter-v2.factory';
+import { ConfigService } from '@nestjs/config';
 
 @Processor(PRODUCT_DISCOUNT_QUEUE)
 export class ProductDiscountProcessor extends WorkerHost {
@@ -11,15 +12,21 @@ export class ProductDiscountProcessor extends WorkerHost {
     private readonly productDiscountService: ProductDiscountService,
     private listFilterFactory: ListFilterV2Factory,
     private logger: DBLogger,
+    private config: ConfigService,
   ) {
     super();
   }
 
   async process(job: Job<any, any, string>, token?: string): Promise<any> {
-    const listFilter = await this.listFilterFactory.create();
-    listFilter.limit = 10;
-    listFilter.offset = 0;
-    await this.productDiscountService.fetchAndApplyDiscountTime(listFilter);
+    let environment = this.config.get('NODE_ENV');
+    let isDevelopment = environment === 'development';
+    if (!isDevelopment) {
+      const listFilter = await this.listFilterFactory.create();
+      listFilter.limit = 10;
+      listFilter.offset = 0;
+      await this.productDiscountService.fetchAndApplyDiscountTime(listFilter);
+    }
+
     return Promise.resolve();
   }
 
