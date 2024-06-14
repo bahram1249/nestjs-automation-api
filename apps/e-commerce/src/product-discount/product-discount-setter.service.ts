37cdb4328fa3d54f9eea96bcd3447579b5e4f21d
bahram rajabi <bahram.rajabiws@gmail.com>
2024-06-15 00:35:23 +0330

    inventory silent update

--- apps/e-commerce/src/product-discount/product-discount-setter.service.ts
+++ apps/e-commerce/src/product-discount/product-discount-setter.service.ts
@@ -84,6 +84,7 @@ export class ProductDiscountSetterService {
           where: {
             id: inventory.id,
           },
+          silent: true,
         },
       );
       return inventory;
@@ -101,6 +102,7 @@ export class ProductDiscountSetterService {
         where: {
           id: inventory.id,
         },
+        silent: true,
       },
     );
 
