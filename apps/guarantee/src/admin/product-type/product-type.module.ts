411011e6578495a2b9b7705f6a34c43225f42f2d
bahram <bahram.rajabiws@gmail.com>
2025-02-22 17:03:17 +0330

    add dependencies model in module

--- apps/guarantee/src/admin/product-type/product-type.module.ts
+++ apps/guarantee/src/admin/product-type/product-type.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProductTypeService } from './product-type.service';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { GSProductType } from '@rahino/database';
+import { GSProductType, Permission, User } from '@rahino/database';
 import { ProductTypeController } from './product-type.controller';
 import { ProductTypeProfile } from './mapper';
 
 @Module({
-  imports: [SequelizeModule.forFeature([GSProductType])],
+  imports: [SequelizeModule.forFeature([GSProductType, User, Permission])],
   controllers: [ProductTypeController],
   providers: [ProductTypeService, ProductTypeProfile],
   exports: [ProductTypeService],
