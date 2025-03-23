a69ac2b34ba9c9860622121097f1a9f94ff1d778
bahram <bahram.rajabiws@gmail.com>
2025-03-23 15:13:40 +0330

    add include for node and nodeCommand in validateAndReturnCartableItem method

--- apps/guarantee/src/cartable/guarantee-traverse/guarantee-traverse.service.ts
+++ apps/guarantee/src/cartable/guarantee-traverse/guarantee-traverse.service.ts
@@ -9,12 +9,17 @@ import * as _ from 'lodash';
 import { ValidateAndReturnCartableItemDto } from './dto';
 import { InjectModel } from '@nestjs/sequelize';
 import {
+  BPMNActivity,
   BPMNNode,
   BPMNNodeCommand,
+  BPMNNodeCommandType,
+  BPMNPROCESS,
   BPMNRequest,
   BPMNRequestState,
 } from '@rahino/localdatabase/models';
 import { QueryOptionsBuilder } from '@rahino/query-filter/sequelize-query-builder';
+import { Op, Sequelize } from 'sequelize';
+import { NodeCommandTypeEnum } from '@rahino/bpmn/modules/node-command-type';
 
 export class GuaranteeTraverseService {
   constructor(
@@ -63,10 +68,79 @@ export class GuaranteeTraverseService {
       new QueryOptionsBuilder().filter({ id: cartableItem.requestId }).build(),
     );
     node = await this.nodeRepository.findOne(
-      new QueryOptionsBuilder().filter({ id: node.id }).build(),
+      new QueryOptionsBuilder()
+        .include([
+          {
+            model: BPMNNodeCommand,
+            as: 'nodeCommands',
+            required: true,
+            where: {
+              [Op.and]: [
+                {
+                  id: dto.nodeCommandId,
+                },
+                Sequelize.where(
+                  Sequelize.fn(
+                    'isnull',
+                    Sequelize.col('nodeCommands.isDeleted'),
+                    0,
+                  ),
+                  {
+                    [Op.eq]: 0,
+                  },
+                ),
+              ],
+            },
+            include: [
+              {
+                model: BPMNNodeCommandType,
+                as: 'nodeCommandType',
+              },
+            ],
+          },
+          {
+            model: BPMNActivity,
+            as: 'fromActivity',
+            include: [
+              {
+                model: BPMNPROCESS,
+                as: 'process',
+              },
+            ],
+          },
+          {
+            model: BPMNActivity,
+            as: 'toActivity',
+            include: [
+              {
+                model: BPMNPROCESS,
+                as: 'process',
+              },
+            ],
+          },
+        ])
+        .filter(
+          Sequelize.where(
+            Sequelize.fn('isnull', Sequelize.col('BPMNNode.isDeleted'), 0),
+            {
+              [Op.eq]: 0,
+            },
+          ),
+        )
+        .filter({ id: node.id })
+
+        .build(),
     );
     nodeCommand = await this.nodeCommandRepository.findOne(
-      new QueryOptionsBuilder().filter({ id: nodeCommand.id }).build(),
+      new QueryOptionsBuilder()
+        .include([
+          {
+            model: BPMNNodeCommandType,
+            as: 'nodeCommandType',
+          },
+        ])
+        .filter({ id: nodeCommand.id })
+        .build(),
     );
 
     return {
