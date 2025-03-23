7cd99a2a51c91c610f3d660bc3fdd4e7d8f11687
bahram <bahram.rajabiws@gmail.com>
2025-03-23 14:51:41 +0330

    add annotations to GuaranteeTraverseDto fields

--- apps/guarantee/src/shared/guarantee-traverse/dto/guarantee-traverse.dto.ts
+++ apps/guarantee/src/shared/guarantee-traverse/dto/guarantee-traverse.dto.ts
@@ -1,6 +1,12 @@
+import { IsNumber } from 'class-validator';
+
 export class GuaranteeTraverseDto {
+  @IsNumber()
   requestStateId: bigint;
+  @IsNumber()
   requestId: bigint;
+  @IsNumber()
   nodeId: number;
+  @IsNumber()
   nodeCommandId: number;
 }
