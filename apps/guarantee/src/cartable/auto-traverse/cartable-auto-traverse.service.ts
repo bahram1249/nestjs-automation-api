f9e8fe9cb7ffb4d2e76ea51aafb2c4d8909008cb
bahram <bahram.rajabiws@gmail.com>
2025-04-01 13:52:40 +0330

    return error message

--- apps/guarantee/src/cartable/auto-traverse/cartable-auto-traverse.service.ts
+++ apps/guarantee/src/cartable/auto-traverse/cartable-auto-traverse.service.ts
@@ -59,7 +59,7 @@ export class CartableAutoTraverseService {
       await transaction.commit();
     } catch (error) {
       await transaction.rollback();
-      throw error;
+      throw new BadRequestException(error.message);
     }
     return {
       result: {
