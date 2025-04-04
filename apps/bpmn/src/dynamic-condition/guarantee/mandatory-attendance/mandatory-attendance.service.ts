a1eb10abf4f0b840f7b839946d27de3b8118db60
bahram <bahram.rajabiws@gmail.com>
2025-04-04 13:02:30 +0330

    change condition with bussiness

--- apps/bpmn/src/dynamic-condition/guarantee/mandatory-attendance/mandatory-attendance.service.ts
+++ apps/bpmn/src/dynamic-condition/guarantee/mandatory-attendance/mandatory-attendance.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/sequelize';
 import { CheckConditionsDto } from '@rahino/bpmn/modules/condition';
 import { ConditionServiceImp } from '@rahino/bpmn/modules/condition/interface';
 import { defaultValueIsNull } from '@rahino/commontools/functions/default-value-isnull';
+import { GSRequestTypeEnum } from '@rahino/guarantee/shared/request-type';
 import { GSProductType, GSRequest } from '@rahino/localdatabase/models';
 import { QueryOptionsBuilder } from '@rahino/query-filter/sequelize-query-builder';
 
@@ -26,7 +27,8 @@ export class MandatoryAttendanceService implements ConditionServiceImp {
         .build(),
     );
     return (
-      defaultValueIsNull(request.productType.mandatoryAttendance, false) == true
+      defaultValueIsNull(request.productType.mandatoryAttendance, false) ==
+        true || request.requestTypeId == GSRequestTypeEnum.Install
     );
   }
 }
