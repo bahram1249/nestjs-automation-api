ec45b91625a62fe9a37da6a0c4494da25dd3edce
bahram <bahram.rajabiws@gmail.com>
2025-04-04 21:46:57 +0330

    fix condition

--- apps/bpmn/src/dynamic-condition/guarantee/non-mandatory-attendance/non-mandatory-attendance.service.ts
+++ apps/bpmn/src/dynamic-condition/guarantee/non-mandatory-attendance/non-mandatory-attendance.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/sequelize';
 import { CheckConditionsDto } from '@rahino/bpmn/modules/condition';
 import { ConditionServiceImp } from '@rahino/bpmn/modules/condition/interface';
 import { defaultValueIsNull } from '@rahino/commontools/functions/default-value-isnull';
+import { GSRequestTypeEnum } from '@rahino/guarantee/shared/request-type';
 import { GSProductType, GSRequest } from '@rahino/localdatabase/models';
 import { QueryOptionsBuilder } from '@rahino/query-filter/sequelize-query-builder';
 
@@ -27,7 +28,7 @@ export class NonMandatoryAttendanceService implements ConditionServiceImp {
     );
     return (
       defaultValueIsNull(request.productType.mandatoryAttendance, false) ==
-      false
+        false && request.requestTypeId != GSRequestTypeEnum.Install
     );
   }
 }
