b8267cdf99e8927f470e6f79afc1c14d1b0a00fe
bahram <bahram.rajabiws@gmail.com>
2025-04-29 21:21:22 +0330

    move home things to home-page-section

--- apps/e-commerce/src/e-commerce.module.ts
+++ apps/e-commerce/src/e-commerce.module.ts
@@ -81,7 +81,7 @@ import { CancellOrderModule } from './admin/cancell-order/cancell-order.module';
 import { UserCommentModule } from './user/comment/comment.module';
 import { ChargingWalletPaymentModule } from './user/charging-wallet-payment/charging-wallet-payment.module';
 import { AdminHomePageModule } from './admin/home-page-section/home-page/home-page.module';
-import { HomePagePhotoModule } from './admin/home-page-photo/home-page-photo.module';
+import { HomePagePhotoModule } from './admin/home-page-section/home-page-photo/home-page-photo.module';
 import { EntityTypeSortModule } from './admin/entity-type-sort/entity-type-sort.module';
 import { HomePhotoModule } from './home-photo/home-photo.module';
 import { HomePageModule } from './home/home.module';
