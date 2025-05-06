7f3a93f15e55d08c17259c4b2b2f44536338dcd6
bahram <bahram.rajabiws@gmail.com>
2025-05-06 09:23:54 +0330

    fix issue

--- apps/guarantee/src/report/income-report/income-report.service.ts
+++ apps/guarantee/src/report/income-report/income-report.service.ts
@@ -91,6 +91,7 @@ export class IncomeReportService {
         {
           model: GSRequest,
           as: 'guaranteeRequest',
+          attributes: [],
           required: true,
         },
       ])
