721b9cd4b2c3e1e85d1d86850a2355954e120ac7
bahram <bahram.rajabiws@gmail.com>
2025-05-22 19:26:01 +0330

    add sms sender in joining operation

--- apps/guarantee/src/anonymous/subscription/subscription.module.ts
+++ apps/guarantee/src/anonymous/subscription/subscription.module.ts
@@ -4,9 +4,29 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { GSSubscription } from '@rahino/localdatabase/models';
 import { SubscriptionController } from './subscription.controller';
 import { LocalizationModule } from 'apps/main/src/common/localization';
+import { BullModule } from '@nestjs/bullmq';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SUBSCRIPTION_SMS_SENDER_QUEUE } from '@rahino/guarantee/job/subscription-sms-sender/constants';
 
 @Module({
-  imports: [LocalizationModule, SequelizeModule.forFeature([GSSubscription])],
+  imports: [
+    LocalizationModule,
+    SequelizeModule.forFeature([GSSubscription]),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        connection: {
+          host: config.get<string>('REDIS_ADDRESS'),
+          port: config.get<number>('REDIS_PORT'),
+          password: config.get<string>('REDIS_PASSWORD'),
+        },
+      }),
+    }),
+    BullModule.registerQueueAsync({
+      name: SUBSCRIPTION_SMS_SENDER_QUEUE,
+    }),
+  ],
   controllers: [SubscriptionController],
   providers: [SubscriptionService],
 })
--- apps/guarantee/src/anonymous/subscription/subscription.service.ts
+++ apps/guarantee/src/anonymous/subscription/subscription.service.ts
@@ -5,13 +5,17 @@ import { SubscriptionDto } from './dto';
 import { LocalizationService } from 'apps/main/src/common/localization';
 import * as _ from 'lodash';
 import { QueryOptionsBuilder } from '@rahino/query-filter/sequelize-query-builder';
-
+import { InjectQueue } from '@nestjs/bullmq';
+import { Queue } from 'bullmq';
+import { SUBSCRIPTION_SMS_SENDER_QUEUE } from '@rahino/guarantee/job/subscription-sms-sender/constants';
 @Injectable()
 export class SubscriptionService {
   constructor(
     @InjectModel(GSSubscription)
     private readonly repository: typeof GSSubscription,
     private readonly localizationService: LocalizationService,
+    @InjectQueue(SUBSCRIPTION_SMS_SENDER_QUEUE)
+    private readonly subscriptionQueue: Queue,
   ) {}
 
   async create(dto: SubscriptionDto) {
@@ -28,6 +32,10 @@ export class SubscriptionService {
     }
 
     const subscription = await this.repository.create({
+      phoneNumber: dto.phoneNumber,
+    });
+
+    await this.subscriptionQueue.add('send-subscription-sms', {
       phoneNumber: dto.phoneNumber,
     });
 
